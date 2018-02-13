e0ebd8e3759d114e6d386e6b2e57a05f6f797e42
Tom Lee <tlee@mapbox.com>
2018-02-13 17:24:27 -0500

    integration test

---
+++ html/index.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var mapboxgl = require('mapbox-gl');
+var instrumentile = require('..');
+
+var TOKEN = 'pk.eyJ1Ijoic2JtYTQ0IiwiYSI6ImNqZG01Y3I5MDBoNDYycGxqZTMyNDRpNXEifQ.YHhxnKFP3dMzYutQGWiG9w';
+
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+mapboxgl.accessToken = TOKEN;
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    collectResourceTiming: true,
+    hash: true,
+    center: [-77.0574, 38.8937],
+    zoom: 12
+});
+
+instrumentile(map, {
+    token: TOKEN,
+    source: 'integrationtest',
+    stub: {
+      events: {
+        push: function(e) {
+          // check if event looks ok
+          var numericProps = ['DNS', 'TCP', 'request', 'response', 'timeTaken'];
+          if ((e.event === 'map.dragend') || (e.event === 'map.click'))
+            numericProps = ['lat', 'lng', 'zoom'];
+          else if (e.event === 'map.load')
+            numericProps = ['DNS', 'TCP', 'appCache', 'lat', 'lng', 'loadtime', 'request', 'response', 'zoom'];
+          if (!numericProps.every(function(p) {
+            var res = (e[p] || (e[p] === 0)) && isNumeric(e[p]);
+            if (!res) {
+              console.error('event ' + e.event + ' property ' + p + ' is not numeric');
+            }
+            return res;
+          }))
+            return;
+          if (!e.source === 'integrationtest')
+            return;
+
+          // note its reception
+          var elemName = e.event.replace('.', '-');
+          if (elemName === 'source-geojson')
+            elemName += '-' + (e.url.indexOf('step1') !== -1 ? 'load' : 'setdata');
+          var elem = document.getElementById(elemName);
+          if (elem)
+            elem.className = 'success';
+        }
+      }
+    }
+});
+
+map.on('load', () => {
+    console.log('- map loaded. loading GeoJSON...');
+    map.addSource('blah', {
+        type: 'geojson',
+        data: 'step1.geojson'
+    });
+
+    window.setTimeout( function(){
+        console.log('- loading second GeoJSON...');
+        map.getSource('blah').setData('step2.geojson');
+    }, 1000);
+});
