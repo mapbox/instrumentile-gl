08fc220a41861762aa9d704fe6a98d31543a2be4
Tom Lee <tlee@mapbox.com>
2018-05-14 13:37:23 -0400

    set mime type on file upload to s3

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,5 +7,5 @@ VERSION="v$(node -e "$JS")"
 
 (cd $(dirname $0)/../ && \
     npm run build && \
-    aws s3 cp --acl=public-read dist/mapbox-gl-instrumentile.min.js s3://mapbox-gl-js/plugins/mapbox-gl-instrumentile/$VERSION/mapbox-gl-instrumentile.js && \
+    aws s3 cp --content-type="application/javascript" --acl=public-read dist/mapbox-gl-instrumentile.min.js s3://mapbox-gl-js/plugins/mapbox-gl-instrumentile/$VERSION/mapbox-gl-instrumentile.js && \
     echo "ok - published $VERSION to https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-instrumentile/$VERSION/mapbox-gl-instrumentile.js" || echo "error")
